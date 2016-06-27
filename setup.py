2ec778ba1cd79bc1f5e96a15868b0441a9bc1768
Matthew Antalek <matthew.antalek@northwestern.edu>
2016-06-27 15:39:59 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    author="Matthew Antalek",
+    name="uniprotmapper",
+    packages=["uniprotmapper"],
+    license="MIT",
+    author_email="matthew.antalek@northwestern.edu",
+    version="0.1dev"
+)
